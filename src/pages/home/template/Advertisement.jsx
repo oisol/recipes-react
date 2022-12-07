b1c67c6602a94b4993ec077071e317026ed2b5dc
Matheus <matheusroca11@gmail.com>
2022-12-06 22:04:59 -0300

    refatorando article tailwind

--- src/pages/home/template/Advertisement.jsx
+++ src/pages/home/template/Advertisement.jsx
@@ -1,7 +1,7 @@
 const Advertisement = () => {
     return (
         <section className={style.section}>
-            
+
             <h2 className={style.title}>Vire um Membro Pro</h2>
 
             <div className={style.mainDiv}>
@@ -40,11 +40,12 @@ const style = {
     title: `font-semibold text-lg`,
     mainDiv: `flex justify-around w-full px-9 mt-8`,
     adCard: `bg-white w-1/5 h-32 rounded-xl p-2 flex flex-col items-center cursor-pointer shadow-3xl`,
-    adCardAlt: `bg-orange-400 w-1/5 h-32 rounded-xl p-2 flex flex-col items-center cursor-pointer shadow-3xl`,
+    adCardAlt: `bg-orange-400 w-1/5 h-32 rounded-xl p-2 flex flex-col items-center cursor-pointer shadow-3xl animate-pulse`,
     cardTitle: `text-center font-semibold text-gray-800`,
     cardText: `text-left break-all overflow-scroll text-xs mt-1 text-gray-700 mx-2`,
     cardTitleAlt: `text-center text-white font-semibold`,
     cardTextAlt: `text-left text-white break-all overflow-scroll text-xs mt-1 mx-2`,
+
     icons: {
         smiley: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-orange-400">
                     <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-2.625 6c-.54 0-.828.419-.936.634a1.96 1.96 0 00-.189.866c0 .298.059.605.189.866.108.215.395.634.936.634.54 0 .828-.419.936-.634.13-.26.189-.568.189-.866 0-.298-.059-.605-.189-.866-.108-.215-.395-.634-.936-.634zm4.314.634c.108-.215.395-.634.936-.634.54 0 .828.419.936.634.13.26.189.568.189.866 0 .298-.059.605-.189.866-.108.215-.395.634-.936.634-.54 0-.828-.419-.936-.634a1.96 1.96 0 01-.189-.866c0-.298.059-.605.189-.866zm2.023 6.828a.75.75 0 10-1.06-1.06 3.75 3.75 0 01-5.304 0 .75.75 0 00-1.06 1.06 5.25 5.25 0 007.424 0z" clipRule="evenodd" />
--- src/pages/home/template/Article.jsx
+++ src/pages/home/template/Article.jsx
@@ -2,23 +2,33 @@ import bg_img from '../../../assets/imgs/food-frame.png'
 
 const Article = () => {
     return (
-        <article className='mt-3 flex sm:justify-center sm:flex-row sm:items-start items-center flex-col'>
+        <article className={style.article}>
 
-            <img src={bg_img} alt="Prato com comida mexicana e tortillas" className='w-2/4 cursor-pointer max-w-md sm:rounded-md'/>
+            <img className={style.image} src={bg_img} alt="Prato com comida mexicana e tortillas"/>
 
-            <div className='sm:w-2/5 flex flex-col sm:items-start items-center p-3'>
-                <h2 className='text-left font-semibold text-sm text-orange-400'>Sobre Nós</h2>
-                <h3 className='text-left font-semibold mt-2 text-xl'>A parte mais essencial de uma<br/>dieta bem balanceada é <span>COMIDA</span></h3>
-                <p className='text-left w-11/12 max-h-52 overflow-scroll mt-4 text-xs'>
+            <div className={style.mainDiv}>
+                <h2 className={style.subtitle}>Sobre Nós</h2>
+                <h3 className={style.title}>A parte mais essencial de uma<br/>dieta bem balanceada é <span>COMIDA</span></h3>
+                <p className={style.text}>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Non doloribus odit eligendi fugiat.
                     cupiditate esse ad doloremque molestiae delectus quasi nemo velit, natus facere mollitia quam.
                     Provident quam officiis aliquam harum, numquam dolor tenetur, facilis inventore, ad recusandae repellat non illum accus.
                 </p>
-                <button className='font-bold text-sm bg-orange-400 text-white rounded-full px-2 py-1 mt-6'>Leia mais</button>
+                <button className={style.buttom}>Leia mais</button>
             </div>
 
         </article>
     )
+}
+
+const style = {
+    article: `mt-3 flex sm:justify-center sm:flex-row sm:items-start items-center flex-col`,
+    image: `w-2/4 cursor-pointer max-w-md sm:rounded-md`,
+    mainDiv: `sm:w-2/5 flex flex-col sm:items-start items-center p-3`,
+    subtitle: `text-left font-semibold text-sm text-orange-400`,
+    title: `text-left font-semibold mt-2 text-xl`,
+    text: `text-left w-11/12 max-h-52 overflow-scroll mt-4 text-xs`,
+    buttom: `font-bold text-sm bg-orange-400 text-white rounded-full px-2 py-1 mt-6 hover:translate-y-1 delay-50 hover:scale-105`
 }
 
 export default Article
