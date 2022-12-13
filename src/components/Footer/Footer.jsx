b8eeb7a2489e3501364e0d7d8b7c2747b25c3ea3
Matheus <matheusroca11@gmail.com>
2022-12-13 20:46:38 -0300

    finalizando footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
-        <footer className="flex lg:flex-row flex-col sm:mt-5 bg-slate-500 h-auto">
+        <footer className="flex lg:flex-row flex-col sm:mt-12 h-auto">
             {/* NEWLETTER */}
-            <div className="bg-white lg:w-1/3 w-full lg:h-44 h-36 pl-4 pb-4 flex flex-col lg:justify-end lg:items-start items-center justify-center">
+            <div className="bg-white lg:w-1/3 w-full lg:h-44 h-36 pl-5 pb-6 flex flex-col lg:justify-end lg:items-start items-center justify-center">
                 <p className="text-2xl font-semibold ">Pronto para cozinhar?</p>
                 <h3 className="text-base font-medium">Assine nossa newsletter semanal!</h3>
 
@@ -15,54 +15,58 @@ const Footer = () => {
             </div>
 
             {/* LINKS RÁPIDOS */}
-            <div className="bg-red-500 lg:w-2/3 w-full lg:h-44 h-48 flex flex-row justify-around flex-wrap">
+            <div className="lg:w-2/3 w-full lg:h-44 h-48 flex flex-row justify-around flex-wrap">
                 {/* QuickLinks */}
                 <div>
-                    <p>Links Rápidos</p>
+                    <p className="text-base text-gray-900 sm:font-medium font-semibold ">Links Rápidos</p>
                     <ul className="pl-4">
                         <li>
-                            <a href="#">
+                            <a className="hover:text-orange-400 font-medium" href="#">
                                 Home
                             </a>
                         </li>
                         <li>
-                            <a href="#">
+                            <a className="hover:text-orange-400 font-medium" href="#">
                                 Link2
                             </a>
                         </li>
                         <li>
-                            <a href="#">
+                            <a className="hover:text-orange-400 font-medium" href="#">
                                 Link3
                             </a>
                         </li>
                     </ul>
                 </div>
+
                 {/* About Us */}
                 <section className="flex flex-col items-center w-1/3 break-all">
-                    <h3>Sobre Nós</h3>
+                    <p className="text-base text-gray-900 sm:font-medium font-semibold ">Sobre Nós</p>
                     <p className="overflow-scroll h-36">Sed non dolor mauris. Sed mi est, ornare ac felis a, volutpat suscipit mi. 
                         Integer in venenatis dolor. Donec sed nibh velit. Maecenas odio sapien, posuere gravida magna vel, porta.
                     </p>
 
                 </section>
+
                 {/* Social Media */}
                 <div>
-                    <li>
+                    <p className="text-base text-gray-900 sm:font-medium font-semibold ">Redes Sociais</p>
+                    <div>
                         <a href="#">
                             Instagram Icon
                         </a>
-                    </li>
-                    <li>
+                    </div>
+                    <div>
                         <a href="#">
                             Twitter Icon
                         </a>
-                    </li>
-                    <li>
+                    </div>
+                    <div>
                         <a href="#">
                             Facebook Icon
                         </a>
-                    </li>
+                    </div>
                 </div>
+
             </div>
         </footer>
     )
