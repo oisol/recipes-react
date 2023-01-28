1d45fa6dc56857ee11068ce27ef6dd452959e034
Matheus <matheusroca11@gmail.com>
2023-01-28 16:46:40 -0300

    backup

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
                             <p className="sm:text-xl font-medium">Tem uma duvida?</p>
                             <h1 className="sm:text-2xl font-semibold">Entre em contato!</h1>
                         </div>
-                        <input className="mt-4 p-2 font-medium rounded-sm outline outline-1 outline-slate-300 focus:outline-none focus:bg-slate-200" type="text" name="name" id="name" placeholder="Seu Nome"/>
+                        <input className="mt-4 p-2 font-medium rounded-sm outline outline-1 outline-slate-300 focus:outline-none focus:bg-slate-200" type="text" name="name" id="name" placeholder="Seu Nome" />
                         <input className="mt-2 p-2 font-medium rounded-sm outline outline-1 outline-slate-300 focus:outline-none focus:bg-slate-200" type="email" name="email" id="email" placeholder="Seu Email"/>
                         <textarea className="mt-3 p-2 rounded-sm outline outline-1 outline-slate-300 focus:outline-none focus:bg-slate-200" name="message" id="message" cols="25" rows="4" placeholder="Sua mensagem aqui"></textarea>
                         <button className="outline outline-2 text-lg font-bold p-2 py-3 mt-4 hover:outline-none rounded-sm hover:bg-orange-300 hover:text-white hover:animate-pulse" type="submit" name="submitButton">Enviar</button>
