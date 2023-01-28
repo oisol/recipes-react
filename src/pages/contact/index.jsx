b74dcffb12652bfbb477dc44ae109a75b206af4f
Matheus <matheusroca11@gmail.com>
2023-01-28 16:21:25 -0300

    estilizando formulario de contato

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -5,20 +5,23 @@ const Contact = () => {
     return (
         <div>
             <Header />
-            <div>
-                <div>
-                    <form action="#">
+            <div className="flex justify-center mt-10">
+
+                <div className="sm:h-96 w-96 flex justify-center">
+                    <form action="#" className="flex flex-col w-3/4 gap-5">
                         <p>Tem uma duvida?</p>
                         <h1>Entre em contato!</h1>
-                        <input type="text" name="name" id="name" />
-                        <input type="email" name="email" id="email" />
-                        <input type="text" name="message" id="message" />
-                        <input type="submit" value="" />
+                        <input type="text" name="name" id="name" placeholder="Seu Nome"/>
+                        <input type="email" name="email" id="email" placeholder="Seu Email"/>
+                        <textarea name="message" id="message" cols="25" rows="4"></textarea>
+                        <button className="outline outline-2 text-lg font-bold p-2 hover:outline-none rounded-sm hover:bg-orange-300 hover:text-white hover:animate-pulse" type="submit" name="submitButton">Enviar</button>
                     </form>
                 </div>
+
                 <div>
                     <img src="" alt="" />
                 </div>
+
             </div>
             <Footer />
         </div>
