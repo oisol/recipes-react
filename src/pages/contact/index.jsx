4837ceb5f578460ef6553720f49cbb01cd4cbbf3
Matheus <matheusroca11@gmail.com>
2023-01-30 23:53:11 -0300

    Success alert e responsividade mobile

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -4,16 +4,21 @@ import Header from "../../components/Header/Header";
 import imagem from "../../assets/imgs/contato.png";
 
 const Contact = () => {
+
+    const successMessage = () => {
+        alert('Sua mensagem foi enviada com sucesso!')
+    };
+
     return (
         <div>
             <Header />
-            <div className="flex justify-center mt-14 mb-16">
+            <div className="flex sm:justify-center items-center sm:flex-row flex-col-reverse mt-14 mb-16">
 
-                <div className="sm:h-96 w-96 flex justify-center">
-                    <form action="#" className="flex flex-col w-3/4">
+                <div className="sm:h-96 h-full sm:w-96 w-11/12 flex justify-center">
+                    <form action="#" method="#POST" className="flex flex-col sm:w-3/4 w-11/12" onSubmit={successMessage}>
                         <div>
-                            <p className="sm:text-xl font-medium">Tem uma duvida?</p>
-                            <h1 className="sm:text-2xl font-semibold">Entre em contato!</h1>
+                            <p className="sm:text-xl text-lg font-medium">Tem uma duvida?</p>
+                            <h1 className="sm:text-2xl text-xl font-semibold">Entre em contato!</h1>
                         </div>
                         <input className="mt-4 p-2 font-medium rounded-sm shadow-md outline-slate-300 focus:outline-none focus:bg-slate-200" type="text" name="name" id="name" placeholder="Seu Nome" required="true"/>
                         <input className="mt-3 p-2 font-medium rounded-sm shadow-md outline-slate-300 focus:outline-none focus:bg-slate-200" type="email" name="email" id="email" placeholder="Seu Email" required="true"/>
@@ -23,7 +28,7 @@ const Contact = () => {
                 </div>
 
                 <div>
-                    <img className="h-96 w-72 object-cover" src={imagem} alt="Pessoas sentadas de frente a um notebook" />
+                    <img className="h-96 w-72 object-cover hidden sm:flex" src={imagem} alt="Pessoas sentadas de frente a um notebook" />
                 </div>
 
             </div>
