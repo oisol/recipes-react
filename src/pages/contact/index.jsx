23c309dd6149f0fcfbc45d0dd697d43b77db74bb
Matheus <matheusroca11@gmail.com>
2023-02-10 18:01:40 -0300

    refatorando Contact

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -12,29 +12,49 @@ const Contact = () => {
     return (
         <div>
             <Header />
-            <div className="flex sm:justify-center items-center sm:flex-row flex-col-reverse mt-14 mb-16">
-
-                <div className="sm:h-96 h-full sm:w-96 w-5/6 flex justify-center">
-                    <form action="#" method="#POST" className="flex flex-col sm:w-3/4 w-11/12" onSubmit={successMessage}>
+            <div className={style.mainDiv}>
+                {/* Formulario */}
+                <div className={style.formMainDiv}>
+                    <form action="#" method="#POST" className={style.formMain} onSubmit={successMessage}>
                         <div>
-                            <p className="sm:text-xl text-lg font-medium">Tem uma duvida?</p>
-                            <h1 className="sm:text-2xl text-xl font-semibold">Entre em contato!</h1>
+                            <p className={style.formMainTitle}>Tem uma duvida?</p>
+                            <h1 className={style.formSubTitle}>Entre em contato!</h1>
                         </div>
-                        <input className="mt-4 p-2 font-medium rounded-sm shadow-md outline-slate-300 focus:outline-none focus:bg-slate-200 hover:bg-slate-100" type="text" name="name" id="name" placeholder="Seu Nome" required="true"/>
-                        <input className="sm:mt-3 mt-5 p-2 font-medium rounded-sm shadow-md outline-slate-300 focus:outline-none focus:bg-slate-200 hover:bg-slate-100" type="email" name="email" id="email" placeholder="Seu Email" required="true"/>
-                        <textarea className="sm:mt-3 mt-5 p-2 rounded-sm shadow-md outline-slate-300 focus:outline-none focus:bg-slate-200 hover:bg-slate-100" name="message" id="message" cols="25" rows="5" placeholder="Sua mensagem aqui" required="true"></textarea>
-                        <button className="outline outline-2 text-lg font-bold p-2 py-3 mt-4 hover:outline-none rounded-sm hover:bg-orange-300 hover:text-white hover:animate-pulse" type="submit" name="submitButton">Enviar</button>
+                        {/* Name */}
+                        <input className={style.formName} type="text" name="name" id="name" placeholder="Seu Nome" required="true"/>
+                        {/* Email */}
+                        <input className={style.formEmail} type="email" name="email" id="email" placeholder="Seu Email" required="true"/>
+                        {/* MSG */}
+                        <textarea className={style.formMessage} name="message" id="message" cols="25" rows="5" placeholder="Sua mensagem aqui" required="true"></textarea>
+                        <button className={style.formButton} type="submit" name="submitButton">Enviar</button>
                     </form>
                 </div>
-
+                
                 <div>
-                    <img className="h-96 w-72 object-cover hidden sm:flex md:p-0 pr-5" src={contactPhoto} alt="Pessoas sentadas de frente a um notebook" />
+                    <img className={style.mainImage} src={contactPhoto} alt="Pessoas sentadas de frente a um notebook" />
                 </div>
 
             </div>
             <Footer />
         </div>
     )
+};
+
+const style = {
+    mainDiv: 'flex sm:justify-center items-center sm:flex-row flex-col-reverse mt-14 mb-16',
+    // Fomulario
+    formMainDiv: 'sm:h-96 h-full sm:w-96 w-5/6 flex justify-center',
+        formMain: 'flex flex-col sm:w-3/4 w-11/12',
+        // Titulo
+        formMainTitle: 'sm:text-xl text-lg font-medium',
+        formSubTitle: 'sm:text-2xl text-xl font-semibold',
+        // Inputs
+        formName: 'mt-4 p-2 font-medium rounded-sm shadow-md outline-slate-300 focus:outline-none focus:bg-slate-200 hover:bg-slate-100',
+        formEmail: 'sm:mt-3 mt-5 p-2 font-medium rounded-sm shadow-md outline-slate-300 focus:outline-none focus:bg-slate-200 hover:bg-slate-100',
+        formMessage: 'sm:mt-3 mt-5 p-2 rounded-sm shadow-md outline-slate-300 focus:outline-none focus:bg-slate-200 hover:bg-slate-100',
+        formButton: 'outline outline-2 text-lg font-bold p-2 py-3 mt-4 hover:outline-none rounded-sm hover:bg-orange-300 hover:text-white hover:animate-pulse',
+    // Image
+        mainImage: 'h-96 w-72 object-cover hidden sm:flex md:p-0 pr-5'
 };
 
 export default Contact;
