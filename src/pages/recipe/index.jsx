14e8f93c2fa133a2243bbc98428ad7f7bf0a58c9
Matheus <matheusroca11@gmail.com>
2023-02-10 18:05:49 -0300

    Fix import para 404

--- src/pages/recipe/index.jsx
+++ src/pages/recipe/index.jsx
@@ -1,5 +1,5 @@
 import data from "../../data";
-
+import { Link } from 'react-router-dom';
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 
